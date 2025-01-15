fac16d4633fb3bd8f1d85b6e58c63c66dbc48a93
Jacob Beacham <bea24011@byui.edu>
2025-01-15 12:05:43 -0500

    Completed the Prep1 and Prep2 Assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade out of 100?");
+        string StringGrade = Console.ReadLine();
+        int Grade = int.Parse(StringGrade);
+
+        if (Grade < 60)
+        {
+            Console.WriteLine("You got an F.");
+        }
+        else if (Grade < 70 && Grade >= 60)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        else if (Grade < 80 && Grade >= 70)
+        {
+            Console.WriteLine("You got a C.");
+        }
+        else if (Grade < 90 && Grade >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else
+        {
+            Console.WriteLine("You got an A!");
+        }
     }
 }
