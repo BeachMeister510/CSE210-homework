cedb3c6d9b8a5efc672b2be74b8a29388e9cb838
Jacob Beacham <bea24011@byui.edu>
2025-01-21 22:08:09 -0700

    Created "Job" and "Resume" class then added code to main to utilize the classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Burger King";
+        job1._jobTitle = "Team Member";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Melaleuca";
+        job2._jobTitle = "Customer Service Rep";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._name = "Jacob Beacham";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
