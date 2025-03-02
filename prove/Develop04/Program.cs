9a7239e59d4a5ce8be574605389b35ef3393a4af
Jacob Beacham <bea24011@byui.edu>
2025-03-01 22:37:36 -0700

    finished Develop 04

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,7 +1,57 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int choice = 0;
+        while (choice != 4)
+        {
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listening Activity");
+            Console.WriteLine("4. Quit");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine("How long in seconds do you want the activty to last?");
+                int length = int.Parse(Console.ReadLine());
+
+                BreathingActivity activity = new BreathingActivity(length);
+                activity.ActivityStart();
+                activity.Breathe();
+                activity.ActivityEnd();
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("How long in seconds do you want the activty to last?");
+                int length = int.Parse(Console.ReadLine());
+
+                ReflectionActivity activity = new ReflectionActivity(length);
+                activity.ActivityStart();
+                activity.PromptUser();
+                activity.Reflection();
+                activity.ActivityEnd();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("How long in seconds do you want the activty to last?");
+                int length = int.Parse(Console.ReadLine());
+
+                ListeningActivity activity = new ListeningActivity(length);
+                activity.ActivityStart();
+                activity.Listen();
+                activity.ActivityEnd();
+
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
     }
 }
